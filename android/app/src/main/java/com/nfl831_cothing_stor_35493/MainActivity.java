98765914991d10ae434e7343afeb72ed7383f311
Crowdbotics <team@crowdbotics.com>
2022-05-29 03:57:02 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/nfl831_cothing_stor_35493/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nfl831_cothing_stor_35493;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "nfl831_cothing_stor_35493";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "nfl831_cothing_stor_35493",
+  url: "https://nfl831_cothing_stor_35493.botics.co",
+  api: "https://nfl831_cothing_stor_35493.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
